41b7181d0ac37600c6d89a4e20392f9b169ff364
waani <kuun993@163.com>
2023-11-24 10:39:20 +0800

    Changes Exec

--- comsol-gen-core/src/main/java/Exec.java
+++ comsol-gen-core/src/main/java/Exec.java
@@ -4,16 +4,23 @@ import com.comsol.model.Model;
 public class Exec {
 
     public static void main(String[] args) {
-        if (args == null || args.length < 2) {
-            throw new IllegalArgumentException();
-        }
-        String taskId = args[0];
-        String configPath = args[1];
         try {
+            if (args == null || args.length < 2) {
+                throw new IllegalArgumentException("IllegalArgument");
+            }
+            String taskId = args[0];
+            String configPath = args[1];
+            System.out.println("*******************************************");
+            System.out.println("submit args");
+            System.out.println("taskId is " + taskId);
+            System.out.println("config path is " + configPath);
+            System.out.println("*******************************************");
             run(taskId, configPath);
         } catch (Exception e) {
             e.printStackTrace(System.out);
             throw new RuntimeException(e.getMessage());
+        } finally {
+            System.out.println("Exec done.");
         }
     }
 
